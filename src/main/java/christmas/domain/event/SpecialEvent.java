c058c5ca15ce4cb9b85363425f3af7abc7809382
kcw0360 <kcw0360@naver.com>
2023-11-11 18:37:45 +0900

    feat(SpecialEvent): 특별 할인 로직 작성

--- src/main/java/christmas/domain/event/SpecialEvent.java
+++ src/main/java/christmas/domain/event/SpecialEvent.java
@@ -1,9 +1,17 @@
 package christmas.domain.event;
 
+import java.util.Set;
+
 public class SpecialEvent implements DiscountEvent {
 
-    @Override
-    public int discount() {
-        return 0;
+    private static final Set<Integer> specialDays = Set.of(3, 10, 17, 24, 25, 31);
+    private static final int SPECIAL_DAY_DISCOUNT_AMOUNT = 1000;
+    private static final int NON_SPECIAL_DAY_DISCOUNT_AMOUNT = 0;
+
+    public int discount(int date) {
+        if (specialDays.contains(date)) {
+            return SPECIAL_DAY_DISCOUNT_AMOUNT;
+        }
+        return NON_SPECIAL_DAY_DISCOUNT_AMOUNT;
     }
 }
