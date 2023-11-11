0b0b75aa62bce9b10a85934e85119313efb3354f
kcw0360 <kcw0360@naver.com>
2023-11-11 18:22:15 +0900

    feat(WeekendEventTest): 주말 할인 로직 작성

--- src/main/java/christmas/domain/event/WeekendEvent.java
+++ src/main/java/christmas/domain/event/WeekendEvent.java
@@ -1,9 +1,25 @@
 package christmas.domain.event;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class WeekendEvent implements DiscountEvent {
 
-    @Override
-    public int discount() {
-        return 0;
+    private static final int DISCOUNT_AMOUNT_UNIT = 2023;
+    private static final int YEAR = 2023;
+    private static final int MONTH = 12;
+    private static final int NON_EVENT_DAY_DISCOUNT_AMOUNT = 0;
+
+    public int discount(int date, int numberOfMainManus) {
+        LocalDate localDate = LocalDate.of(YEAR, MONTH, date);
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+        String day = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN);
+
+        if (weekend.contains(day)) {
+            return DISCOUNT_AMOUNT_UNIT * numberOfMainManus;
+        }
+        return NON_EVENT_DAY_DISCOUNT_AMOUNT;
     }
 }
