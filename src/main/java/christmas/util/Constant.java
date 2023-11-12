400958525e8da11769773d64052609f208a29b30
kcw0360 <kcw0360@naver.com>
2023-11-12 22:14:24 +0900

    feat(Constant): 상수 관리

---
+++ src/main/java/christmas/util/Constant.java
@@ -0,0 +1,15 @@
+package christmas.util;
+
+public class Constant {
+
+    public static final int DATE_MINIMUM_RANGE = 1;
+    public static final int DATE_MAXIMUM_RANGE = 31;
+    public static final int MENU_MINIMUM_RANGE = 1;
+    public static final int MENU_MAXIMUM_RANGE = 20;
+    public static final int TOTAL_NUMBER_OF_MENUS = 20;
+
+    public static final String DELIMITER_COMMA = ",";
+    public static final String DELIMITER_HYPHEN = "-";
+    public static final String NUMBER_PATTERN = "^\\d{1,2}$";
+    public static final String MENU_INPUT_FORMAT_PATTERN = "\\S{1,}-\\d{1,2}";
+}
