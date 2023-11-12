6090a491a5b31a897088192e638b01ad3470ad53
kcw0360 <kcw0360@naver.com>
2023-11-12 20:47:19 +0900

    test(DateValidatorTest): 테스트 코드 수정

--- src/test/java/christmas/util/validator/DateValidatorTest.java
+++ src/test/java/christmas/util/validator/DateValidatorTest.java
@@ -5,6 +5,7 @@ import static christmas.util.Constant.*;
 import static christmas.util.ErrorMessage.*;
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,17 +13,19 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class DateValidatorTest {
 
+    private DateValidator validator;
+
+    @BeforeEach
+    void beforeEach() {
+        this.validator = DateValidator.getValidator();
+    }
+
     @ParameterizedTest
     @DisplayName("날짜 검증 - 입력 날짜가 숫자인지 확인")
     @ValueSource(strings = {"일", "one", "", "1 "})
     void validateNumber(String input) {
-        DateValidator validator = DateValidator.getValidator();
-
-        assertThatCode(() -> validator
-                .validateNumber("0", NUMBER_PATTERN, INVALID_DATE.getMessage()))
-                .doesNotThrowAnyException();
-        assertThatThrownBy(() -> validator
-                .validateNumber(input, NUMBER_PATTERN, INVALID_DATE.getMessage()))
+        assertThatCode(() -> validator.validate("1")).doesNotThrowAnyException();
+        assertThatThrownBy(() -> validator.validate(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
@@ -30,17 +33,11 @@ class DateValidatorTest {
     @Test
     @DisplayName("날짜 검증 - 1 ~ 31일 사이의 날짜인지 확인")
     void validateNumberRange() {
-        DateValidator validator = DateValidator.getValidator();
-
-        assertThatCode(() -> validator
-                .validateNumberRange(31, DATE_MINIMUM_RANGE, DATE_MAXIMUM_RANGE, INVALID_DATE.getMessage()))
-                .doesNotThrowAnyException();
-        assertThatThrownBy(() -> validator
-                .validateNumberRange(0, DATE_MINIMUM_RANGE, DATE_MAXIMUM_RANGE, INVALID_DATE.getMessage()))
+        assertThatCode(() -> validator.validate("31")).doesNotThrowAnyException();
+        assertThatThrownBy(() -> validator.validate("0"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        assertThatThrownBy(() -> validator
-                .validateNumberRange(32, DATE_MINIMUM_RANGE, DATE_MAXIMUM_RANGE, INVALID_DATE.getMessage()))
+        assertThatThrownBy(() -> validator.validate("32"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
