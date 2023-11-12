813b8b304cb08645a13baf9aa51c290dd1975412
kcw0360 <kcw0360@naver.com>
2023-11-12 16:39:45 +0900

    feat(DateValidator): Validator 상속 후 정적 팩토리 메서드 작성

---
+++ src/main/java/christmas/util/validator/DateValidator.java
@@ -0,0 +1,8 @@
+package christmas.util.validator;
+
+public class DateValidator extends Validator {
+
+    public static DateValidator getValidator() {
+        return new DateValidator();
+    }
+}
