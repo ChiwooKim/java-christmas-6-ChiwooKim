6502f3e296afdaf05f9a722a120e025825ff85ea
kcw0360 <kcw0360@naver.com>
2023-11-12 20:46:52 +0900

    feat(DateValidator): 한번에 검증할 수 있도록 메서드 작성

--- src/main/java/christmas/util/validator/DateValidator.java
+++ src/main/java/christmas/util/validator/DateValidator.java
@@ -1,8 +1,17 @@
 package christmas.util.validator;
 
+import static christmas.util.Constant.*;
+import static christmas.util.ErrorMessage.*;
+
 public class DateValidator extends Validator {
 
     public static DateValidator getValidator() {
         return new DateValidator();
+    }
+
+    public void validate(String input) {
+        validateInputPattern(input, NUMBER_PATTERN, INVALID_DATE.getMessage());
+        int date = Integer.parseInt(input);
+        validateNumberRange(date, DATE_MINIMUM_RANGE, DATE_MAXIMUM_RANGE, INVALID_DATE.getMessage());
     }
 }
