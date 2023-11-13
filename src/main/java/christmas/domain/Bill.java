c9a8a4a2937a6dac77598cee649ced2918a8ef84
kcw0360 <kcw0360@naver.com>
2023-11-13 15:30:01 +0900

    feat(Bill): 디저트 수, 메인 메뉴 수 추가 후 로직 수정

--- src/main/java/christmas/domain/Bill.java
+++ src/main/java/christmas/domain/Bill.java
@@ -4,25 +4,56 @@ import java.util.Map;
 
 public class Bill {
 
+    private int numberOfDessert;
+    private int numberOfMain;
+    private int totalAmount;
+
+    public Bill() {
+        this.numberOfDessert = 0;
+        this.numberOfMain = 0;
+        this.totalAmount = 0;
+    }
+
     public static Bill getBill() {
         return new Bill();
     }
 
-    public int calculate(Map<String, Integer> orderInfo) {
+    public void calculate(Map<String, Integer> orderInfo) {
         int totalAmount = 0;
         for (String menu : orderInfo.keySet()) {
-            totalAmount += getPrice(menu) * orderInfo.get(menu);
+            this.totalAmount += getPrice(menu, orderInfo) * orderInfo.get(menu);
         }
-        return totalAmount;
     }
 
-    private int getPrice(String menu) {
+    private int getPrice(String menu, Map<String, Integer> orderInfo) {
         for (Menu type : Menu.values()) {
             if (type.getMenuName().contains(menu)) {
                 int index = type.getMenuName().indexOf(menu);
+                checkType(type, menu, orderInfo);
                 return type.getMenuPrice().get(index);
             }
         }
         return 0;
+    }
+
+    private void checkType(Menu type, String menu, Map<String, Integer> orderInfo) {
+        if (type == Menu.DESSERT) {
+            this.numberOfDessert += orderInfo.get(menu);
+        }
+        if (type == Menu.MAIN) {
+            this.numberOfMain += orderInfo.get(menu);
+        }
+    }
+
+    public int getNumberOfDessert() {
+        return numberOfDessert;
+    }
+
+    public int getNumberOfMain() {
+        return numberOfMain;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
     }
 }
