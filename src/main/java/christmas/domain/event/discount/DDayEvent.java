96e668e9db73ac1089079ce21345c7262d5a19da
kcw0360 <kcw0360@naver.com>
2023-11-14 00:22:56 +0900

    chore(DDayEvent): 변수명 수정

--- src/main/java/christmas/domain/event/discount/DDayEvent.java
+++ src/main/java/christmas/domain/event/discount/DDayEvent.java
@@ -5,7 +5,7 @@ public class DDayEvent implements DiscountEvent {
     private static final int FIRST_DAY_DATE = 1;
     private static final int FIRST_DAY_DISCOUNT = 1000;
     private static final int ADDITIONAL_DISCOUNT_AMOUNT = 100;
-    private static final int NON_EVENT_DAY = 25;
+    private static final int LAST_EVENT_DAY = 25;
     private static final int NON_EVENT_DAY_DISCOUNT_AMOUNT = 0;
 
     public String getDDayEventName() {
@@ -13,7 +13,7 @@ public class DDayEvent implements DiscountEvent {
     }
 
     public int discount(int date) {
-        if (date > NON_EVENT_DAY) {
+        if (date > LAST_EVENT_DAY) {
             return NON_EVENT_DAY_DISCOUNT_AMOUNT;
         }
         return FIRST_DAY_DISCOUNT + ((date - FIRST_DAY_DATE) * ADDITIONAL_DISCOUNT_AMOUNT);
