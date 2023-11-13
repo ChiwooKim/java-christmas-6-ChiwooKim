cf913c41a8fba26f209d74fdc9981454de8578b3
kcw0360 <kcw0360@naver.com>
2023-11-13 16:40:15 +0900

    feat(SpecialEvent): 이벤트 이름 호출 메서드 작성

--- src/main/java/christmas/domain/event/discount/SpecialEvent.java
+++ src/main/java/christmas/domain/event/discount/SpecialEvent.java
@@ -8,6 +8,10 @@ public class SpecialEvent implements DiscountEvent {
     private static final int SPECIAL_DAY_DISCOUNT_AMOUNT = 1000;
     private static final int NON_SPECIAL_DAY_DISCOUNT_AMOUNT = 0;
 
+    public String getSpecialEventName() {
+        return "특별 할인";
+    }
+
     public int discount(int date) {
         if (specialDays.contains(date)) {
             return SPECIAL_DAY_DISCOUNT_AMOUNT;
