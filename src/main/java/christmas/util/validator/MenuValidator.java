277593998cba0d3bfc354f8e00939977dddddf19
kcw0360 <kcw0360@naver.com>
2023-11-14 21:12:40 +0900

    feat(MenuValidator): 변수 추가 후 로직 수정

--- src/main/java/christmas/util/validator/MenuValidator.java
+++ src/main/java/christmas/util/validator/MenuValidator.java
@@ -11,76 +11,71 @@ import java.util.Set;
 
 public class MenuValidator extends Validator {
 
+    private final Set<String> checkMenus;
+    private int numberOfBeverages;
+    private int totalNumber;
+
+    public MenuValidator() {
+        this.checkMenus = new HashSet<>();
+        this.numberOfBeverages = 0;
+        this.totalNumber = 0;
+    }
+
     public static MenuValidator getValidator() {
         return new MenuValidator();
     }
 
     public void validate(String input) {
         List<String> menus = Arrays.asList(input.split(DELIMITER_COMMA));
-        Set<String> checkMenus = new HashSet<>();
-        int numberOfBeverages = 0;
-        int totalNumber = 0;
-        validateIterator(menus, checkMenus, numberOfBeverages, totalNumber);
+        validateIterator(menus);
+        validateDuplication(menus);
+        validateOnlyBeverage(menus);
+        validateTotalNumber();
     }
 
-    private void validateIterator(List<String> menus, Set<String> checkMenus, int numberOfBeverages, int totalNumber) {
+    private void validateIterator(List<String> menus) {
         for (String menu : menus) {
             validateInputPattern(menu, MENU_INPUT_FORMAT_PATTERN, INVALID_MENU.getMessage());
             String[] menuInfo = menu.split(DELIMITER_HYPHEN);
-
-            String menuName = menuInfo[0];
-            numberOfBeverages = getNumberOfBeverages(checkMenus, numberOfBeverages, menuName);
-
-            int numberOfMenus = Integer.parseInt(menuInfo[1]);
-            totalNumber = getTotalNumber(totalNumber, numberOfMenus);
+            getNumberOfBeverages(menuInfo[0]);
+            getTotalNumber(Integer.parseInt(menuInfo[1]));
         }
-        validateDuplication(menus, checkMenus);
-        validateOnlyBeverage(menus, numberOfBeverages);
-        validateTotalNumber(totalNumber);
     }
 
-    private int getTotalNumber(int totalNumber, int numberOfMenus) {
+    private void getTotalNumber(int numberOfMenus) {
         validateNumberRange(numberOfMenus, MENU_MINIMUM_RANGE, MENU_MAXIMUM_RANGE, INVALID_MENU.getMessage());
         totalNumber += numberOfMenus;
-        return totalNumber;
     }
 
-    private int getNumberOfBeverages(Set<String> checkMenus, int numberOfBeverages, String menuName) {
+    private void getNumberOfBeverages(String menuName) {
         validateNoMenu(menuName);
         checkMenus.add(menuName);
         if (Menu.BEVERAGE.getMenuName().contains(menuName)) {
             numberOfBeverages++;
         }
-        return numberOfBeverages;
     }
 
-    private void validateOnlyBeverage(List<String> menus, int numberOfBeverages) {
+    private void validateOnlyBeverage(List<String> menus) {
         if (menus.size() == numberOfBeverages) {
             throw new IllegalArgumentException(INVALID_ORDER.getMessage());
         }
     }
 
-    private void validateTotalNumber(int totalNumber) {
+    private void validateTotalNumber() {
         if (totalNumber > TOTAL_NUMBER_OF_MENUS) {
             throw new IllegalArgumentException(INVALID_TOTAL_NUMBER_OF_MENUS.getMessage());
         }
     }
 
-    private void validateDuplication(List<String> menus, Set<String> duplication) {
-        if (menus.size() != duplication.size()) {
+    private void validateDuplication(List<String> menus) {
+        if (menus.size() != checkMenus.size()) {
             throw new IllegalArgumentException(INVALID_MENU.getMessage());
         }
     }
 
     private void validateNoMenu(String menu) {
-        boolean exist = false;
-        for (Menu type : Menu.values()) {
-            if (type.getMenuName().contains(menu)) {
-                exist = true;
-                break;
-            }
-        }
-        if (!exist) {
+        boolean existMenu = Menu.isExistMenu(menu);
+        if (!existMenu) {
             throw new IllegalArgumentException(INVALID_MENU.getMessage());
         }
     }
