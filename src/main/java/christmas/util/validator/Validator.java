226d72c7c0bfe811bbeec095eab304fce3760884
kcw0360 <kcw0360@naver.com>
2023-11-15 21:50:56 +0900

    feat(Validator): 상속을 위한 검증 메서드 validate 추가

--- src/main/java/christmas/util/validator/Validator.java
+++ src/main/java/christmas/util/validator/Validator.java
@@ -4,6 +4,8 @@ import java.util.regex.Pattern;
 
 public abstract class Validator {
 
+    protected abstract void validate(String input);
+
     public void validateInputPattern(String input, String regex, String error) {
         Pattern pattern = Pattern.compile(regex);
         if (!pattern.matcher(input).matches()) {
