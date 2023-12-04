b4c5a8b9059a3a5c2cdb4e0aa9d598654dc24d58
kcw0360 <kcw0360@naver.com>
2023-12-04 23:00:01 +0900

    chore(ViewMessage): 메시지 수정

--- src/main/java/christmas/view/ViewMessage.java
+++ src/main/java/christmas/view/ViewMessage.java
@@ -3,7 +3,7 @@ package christmas.view;
 public enum ViewMessage {
     READ_DATE("안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.%n" +
             "%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)%n"),
-    READ_MENU("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+    READ_MENU("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)%n"),
     EVENT_PREVIEW("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n"),
     ORDER_MENU("%n<주문 메뉴>%n"),
     TOTAL_AMOUNT("%n<할인 전 총주문 금액>%n"),
