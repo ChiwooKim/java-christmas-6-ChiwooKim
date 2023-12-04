fbea10e32b2d292302e0cb95aff7d82c03bda0a4
kcw0360 <kcw0360@naver.com>
2023-12-04 16:55:25 +0900

    feat(EventController): 이벤트 날짜 입력 로직 작성

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,8 +1,32 @@
 package christmas.controller;
 
+import christmas.domain.EventDay;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class EventController {
 
-    public void hold() {
+    private final InputView inputView;
+    private final OutputView outputView;
 
+    public EventController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void hold() {
+        EventDay eventDay = getEventDay();
+    }
+
+    private EventDay getEventDay() {
+        outputView.printReadDate();
+        while (true) {
+            try {
+                int date = inputView.readDate();
+                return EventDay.from(date);
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
     }
 }
