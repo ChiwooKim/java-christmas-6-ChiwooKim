d27b3a5d2718b4bc11add4fb3cf1a56655b65c0d
kcw0360 <kcw0360@naver.com>
2023-12-04 16:52:46 +0900

    feat(EventDay): 이벤트 날짜에 관한 객체 생성

---
+++ src/main/java/christmas/domain/EventDay.java
@@ -0,0 +1,28 @@
+package christmas.domain;
+
+import christmas.exception.EventException;
+import java.time.LocalDate;
+
+public class EventDay {
+
+    public static final int DATE_MIN_RANGE = 1;
+    public static final int DATE_MAX_RANGE = 31;
+    private static final int YEAR = 2023;
+    private static final int MONTH = 12;
+    private final LocalDate localDate;
+
+    private EventDay(int date) {
+        validateRange(date);
+        this.localDate = LocalDate.of(YEAR, MONTH, date);
+    }
+
+    public static EventDay from(int date) {
+        return new EventDay(date);
+    }
+
+    private void validateRange(int date) {
+        if (date < DATE_MIN_RANGE || DATE_MAX_RANGE < date) {
+            throw EventException.INVALID_DATE.makeException();
+        }
+    }
+}
