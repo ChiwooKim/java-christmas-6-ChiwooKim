bd9c82a59f1cde734add2e6d84d0d50efb421d6a
kcw0360 <kcw0360@naver.com>
2023-12-06 21:01:38 +0900

    feat(OutputView): 출력 메소드 작성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,13 +2,15 @@ package christmas.view;
 
 import static christmas.view.ViewMessage.*;
 
-import christmas.domain.EventDay;
-import christmas.domain.Order;
-import java.util.stream.Collectors;
+import christmas.domain.Bill;
+import christmas.domain.event.Benefits;
+import christmas.domain.event.giveaway.Giveaway;
 
 public class OutputView {
 
     private static final int MONTH = 12;
+    private static final int MIN_AMOUNT = 0;
+    private static final String NO_GIVEAWAY = "없음";
 
     public void printErrorMessage(IllegalArgumentException e) {
         System.out.println(e.getMessage());
@@ -22,11 +24,75 @@ public class OutputView {
         System.out.printf(READ_MENU.getMessage());
     }
 
-    public void printOrderMenu(Order order, EventDay eventDay) {
-        System.out.printf(EVENT_PREVIEW.getMessage(), MONTH, eventDay.getLocalDate().getDayOfMonth());
+    public void printResult(Bill bill) {
+        printOrderMenu(bill);
+        printNoDiscountAmount(bill);
+        printGiveaway(bill);
+        printBenefits(bill);
+        printBenefitsAmount(bill);
+        printAmountOfPayment(bill);
+        printBadge(bill);
+    }
+
+    private void printOrderMenu(Bill bill) {
+        System.out.printf(EVENT_PREVIEW.getMessage(), MONTH, bill.getDate());
         System.out.printf(ORDER_MENU.getMessage());
-        order.getOrder().keySet()
-                .forEach(menu -> System.out.printf(NAME_AND_NUMBER.getMessage(),
-                        menu.getName(), order.getOrder().get(menu)));
+        bill.getOrderConfirmation().getOrderConfirmation()
+                .forEach(order -> System.out.printf(NAME_AND_NUMBER.getMessage(),
+                        order.getMenu().getName(), order.getCount()));
+    }
+
+    private void printNoDiscountAmount(Bill bill) {
+        System.out.printf(TOTAL_AMOUNT.getMessage());
+        System.out.printf(CURRENCY_UNIT.getMessage(),
+                bill.getOrderConfirmation().getTotalPrice());
+    }
+
+    private void printGiveaway(Bill bill) {
+        System.out.printf(GIVEAWAY.getMessage());
+        Giveaway giveaway = bill.getBenefits().getGiveaway();
+        if (giveaway.name().equals(NO_GIVEAWAY)) {
+            System.out.printf(NONE.getMessage());
+            return;
+        }
+        System.out.printf(NAME_AND_NUMBER.getMessage(),
+                giveaway.name(), giveaway.quantity());
+    }
+
+    private void printBenefits(Bill bill) {
+        System.out.printf(BENEFIT_DETAILS.getMessage());
+        Benefits benefits = bill.getBenefits();
+        if (benefits.getTotalDiscount() == MIN_AMOUNT) {
+            System.out.printf(NONE.getMessage());
+            return;
+        }
+        for (int i = 0; i < benefits.getEventDiscount().size(); i++) {
+            System.out.printf(NAME_AND_CURRENCY_UNIT.getMessage(),
+                    benefits.getEventNames().get(i),
+                    benefits.getEventDiscount().get(i));
+        }
+    }
+
+    private void printBenefitsAmount(Bill bill) {
+        System.out.printf(TOTAL_DISCOUNT_AMOUNT.getMessage());
+        int totalDiscount = bill.getBenefits().getTotalDiscount();
+        if (totalDiscount == MIN_AMOUNT) {
+            System.out.printf(CURRENCY_UNIT.getMessage(), totalDiscount);
+            return;
+        }
+        System.out.printf(MINUS_CURRENCY_UNIT.getMessage(), totalDiscount);
+    }
+
+    private void printAmountOfPayment(Bill bill) {
+        System.out.printf(AMOUNT_OF_PAYMENT.getMessage());
+        int totalDiscount =
+                bill.getBenefits().getTotalDiscount() - bill.getBenefits().getGiveaway().price();
+        System.out.printf(CURRENCY_UNIT.getMessage(),
+                bill.getOrderConfirmation().getTotalPrice() - totalDiscount);
+    }
+
+    public void printBadge(Bill bill) {
+        System.out.printf(EVENT_BADGE.getMessage(), MONTH);
+        System.out.printf(bill.getBadge());
     }
 }
