f0b406f81c2bf7e1f251f018ed316bd51e662d71
kcw0360 <kcw0360@naver.com>
2023-12-06 20:53:17 +0900

    feat(EventController): 메인 로직 작성

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,7 +1,10 @@
 package christmas.controller;
 
+import christmas.domain.Bill;
 import christmas.domain.EventDay;
-import christmas.domain.Order;
+import christmas.domain.order.Orders;
+import christmas.domain.order.OrderConfirmation;
+import christmas.service.EventService;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 import java.util.List;
@@ -10,15 +13,19 @@ public class EventController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final EventService eventService;
 
     public EventController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.eventService = new EventService();
     }
 
     public void hold() {
         EventDay eventDay = getEventDay();
-        Order order = getMenus();
+        OrderConfirmation orderConfirmation = getOrder();
+        Bill bill = eventService.getBenefits(eventDay, orderConfirmation);
+        outputView.printResult(bill);
     }
 
     private EventDay getEventDay() {
@@ -33,12 +40,12 @@ public class EventController {
         }
     }
 
-    private Order getMenus() {
+    private OrderConfirmation getOrder() {
         outputView.printReadMenu();
         while (true) {
             try {
                 List<String> menus = inputView.readMenus();
-                return Order.from(menus);
+                return eventService.confirmOrder(Orders.from(menus));
             } catch (IllegalArgumentException e) {
                 outputView.printErrorMessage(e);
             }
