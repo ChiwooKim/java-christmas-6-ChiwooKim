98f3fb05fbb1751aa17a6197d5e6b80644d4e390
kcw0360 <kcw0360@naver.com>
2023-12-06 18:04:16 +0900

    feat(Order): 메뉴 이름과 수 담당 객체 생성

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,78 +1,18 @@
 package christmas.domain;
 
-import static christmas.exception.EventException.*;
-
 import christmas.domain.menu.Menu;
-import christmas.domain.menu.MenuType;
-import java.util.EnumMap;
-import java.util.List;
 
 public class Order {
 
-    private static final int MENU_MIN_RANGE = 1;
-    private static final int MENU_MAX_RANGE = 20;
-    private static final int TOTAL_MAX_COUNT = 20;
-    private final EnumMap<Menu, Integer> order = new EnumMap<>(Menu.class);
+    private final Menu menu;
+    private final int count;
 
-    private Order(List<String> menus) {
-        menus.forEach(this::addMenu);
-        validateOrder();
+    public Order(Menu menu, int count) {
+        this.menu = menu;
+        this.count = count;
     }
 
-    public static Order from(List<String> menus) {
-        return new Order(menus);
-    }
-
-    private void addMenu(String menu) {
-        String[] nameAndCount = menu.split("-");
-        Menu menuName = Menu.from(nameAndCount[0]);
-        int count = Integer.parseInt(nameAndCount[1]);
-        validateMenu(menuName, count);
-        order.put(menuName, count);
-    }
-
-    private void validateMenu(Menu menu, int count) {
-        validateDuplication(menu);
-        validateRange(count);
-    }
-
-    private void validateDuplication(Menu menu) {
-        if (order.containsKey(menu)) {
-            throw INVALID_MENU.makeException();
-        }
-    }
-
-    private void validateRange(int count) {
-        if (count < MENU_MIN_RANGE || MENU_MAX_RANGE < count) {
-            throw INVALID_ORDER.makeException();
-        }
-    }
-
-    private void validateOrder() {
-        validateTotalCount();
-        validateOnlyDrink();
-    }
-
-    private void validateTotalCount() {
-        int totalMenuCount = order.values().stream()
-                .mapToInt(Integer::intValue)
-                .sum();
-        if (TOTAL_MAX_COUNT < totalMenuCount) {
-            throw INVALID_TOTAL_NUMBER_OF_MENUS.makeException();
-        }
-    }
-
-    private void validateOnlyDrink() {
-        boolean exist = order.keySet().stream()
-                .dropWhile(menu -> menu.isDrink(MenuType.DRINK))
-                .findAny()
-                .isEmpty();
-        if (exist) {
-            throw INVALID_ORDER.makeException();
-        }
-    }
-
-    public EnumMap<Menu, Integer> getOrder() {
-        return order;
+    public int getPrice() {
+        return menu.getPrice() * count;
     }
 }
