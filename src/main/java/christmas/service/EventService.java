af51794feaca57f9cdaa74dca84278148d81e003
kcw0360 <kcw0360@naver.com>
2023-12-06 20:53:40 +0900

    feat(EventService): 비즈니스 로직 작성

--- src/main/java/christmas/service/EventService.java
+++ src/main/java/christmas/service/EventService.java
@@ -1,11 +1,23 @@
 package christmas.service;
 
-import christmas.domain.Orders;
-import christmas.domain.menu.OrderConfirmation;
+import christmas.domain.Bill;
+import christmas.domain.EventDay;
+import christmas.domain.event.Badge;
+import christmas.domain.event.Benefits;
+import christmas.domain.order.Orders;
+import christmas.domain.order.OrderConfirmation;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventService {
-
     public OrderConfirmation confirmOrder(Orders orders) {
         return orders.confirmOrder();
+    }
+
+    public Bill getBenefits(EventDay eventDay, OrderConfirmation orderConfirmation) {
+        List<String> details = new ArrayList<>();
+        Benefits benefits = Benefits.of(eventDay, orderConfirmation);
+        String badge = Badge.getBadge(benefits.getTotalDiscount());
+        return Bill.of(eventDay, orderConfirmation, benefits, badge);
     }
 }
